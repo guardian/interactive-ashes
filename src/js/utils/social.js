c401adfa135dc6f7649c861c5ced6295b0f607ec
czapponi <littleark@gmail.com>
2015-07-08 11:26:04 +0100

    added custom image for social

--- src/js/utils/social.js
+++ src/js/utils/social.js
@@ -9,11 +9,11 @@ function ShareButtons(selector) {
         var twitterBaseUrl = "https://twitter.com/intent/tweet?text=";
         var facebookBaseUrl = "https://www.facebook.com/dialog/feed?display=popup&app_id=741666719251986&link=";
         var pageUrl = "http://gu.com/p/4af3t";
-        var shareImage = "pic.twitter.com/XXXXX";
-        var sharePNG = "http://interactive.guim.co.uk/2015/07/the-ashes/imgs/au.jpg";
+        var shareImage = "pic.twitter.com/Y0gcJbhhJT";
+        var sharePNG = "http://interactive.guim.co.uk/2015/07/the-ashes/imgs/overtime.jpg";
 
-        var pageMessage = "#Ashes2015 interactive: A history of the Ashes"+ " " + 
-                pageUrl + " " + shareImage;
+        var pageMessage = "Over time: #Ashes history in charts"+ " " + 
+                pageUrl + " #Ashes2015 " + " " + shareImage;
         
         var shareWindow = "";
         var network = e.currentTarget.getAttribute('data-source'); 
